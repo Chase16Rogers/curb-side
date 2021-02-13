df013dc75ea467686a6b414f625b8acc78176007
Chase16Rogers <chase16rogers@gmail.com>
2021-02-12 17:37:42 -0700

    Fixy fixy bug in server

--- curb-side.client/src/services/GoogleMapsService.js
+++ curb-side.client/src/services/GoogleMapsService.js
@@ -7,21 +7,8 @@ import { api } from './AxiosService'
 class GoogleMapsService {
   async findNearMe(address) {
     const data = address.split(' ').join('+')
-    const res = await api.get('api/businesses/' + data)
+    const res = await api.get('api/businesses/find/' + data)
     AppState.businesses = res.data
   }
-  // async findByDistance(address) {
-  //   const businesses = await api.get('api/businesses')
-  //   let allAddresses = ''
-  //   businesses.data.forEach(b => {
-  //     const googleAddress = b.address.split(' ').join('+')
-  //     allAddresses = allAddresses + googleAddress + '|'
-  //   })
-  //   allAddresses.split('').pop().join('')
-  //   address = address.split(' ').join('+')
-  //   const res = await googleApi.get('json?origin=' + address + '&destination=' + allAddresses + '&key=AIzaSyDoW1Uw5M4sp85tE_yn6p0X5raQ8D-VWZM')
-  //   AppState.googleMaps = res.data
-  //   console.log(res.data)
-  // }
 }
 export const googleMapsService = new GoogleMapsService()
