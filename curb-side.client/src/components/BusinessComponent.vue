c4bea3c9f35c5eacc44b0d6fdd0466c7df634d00
Chase16Rogers <chase16rogers@gmail.com>
2021-02-12 23:51:04 -0700

    accepted

--- curb-side.client/src/components/BusinessComponent.vue
+++ curb-side.client/src/components/BusinessComponent.vue
@@ -1,13 +1,8 @@
 <template>
   <div
-<<<<<<< HEAD
-    class="row my-4 elevation-3 round hover-el-5 business-component bg-white hover-topleft justify-content-between pointer"
-    @click="pushTo()"
-=======
     class="row py-3 pl-3 lines justify-content-between pointer"
     :class="state.color"
     @click="businessManager()"
->>>>>>> d440d577e6ad0dee89c4c1d59dad38c8fd632bb2
   >
     <div class="col-2 p-0">
       <img class="img-fluid square-right round" :src="buizProp.logo" />
